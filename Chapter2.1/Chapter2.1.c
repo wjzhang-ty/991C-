0e7d07f66b46f462c6bb190f2165510b96219e70
wjzhang <2426477795@qq.com>
2020-05-08 17:35:19 +0800

    课后题1，暂存一下回家做

--- Chapter2.1/Chapter2.1.c
+++ Chapter2.1/Chapter2.1.c
@@ -1,7 +1,17 @@
-﻿// Chapter2.1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿/*
+    已知程度为n的线性表A采用顺序存储结构。
+    请写一算法，找出该线性表中值最小的数据元素，给出该元素在表中的位置
+*/
 
 #include <iostream>
+
+#define MaxSize 100
+
+// 顺序表结构体
+typedef struct {
+    int datas[MaxSize];
+    int length;
+}SeqList;
 
 int main()
 {
