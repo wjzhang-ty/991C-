c17931ecbf5874be96a49d025ec723c1288439d9
wjzhang <2426477795@qq.com>
2020-05-09 17:43:52 +0800

    任务二回家研究研究

--- Chapter2.1/Chapter2.1.c
+++ Chapter2.1/Chapter2.1.c
@@ -1,9 +1,10 @@
 ﻿/*
-    已知程度为n的线性表A采用顺序存储结构。
+    已知长度为n的线性表A采用顺序存储结构。
     请写一算法，找出该线性表中值最小的数据元素，给出该元素在表中的位置
 */
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MaxSize 100
 
@@ -13,7 +14,33 @@ typedef struct {
     int length;
 }SeqList;
 
+void initSeq(SeqList seq[],int len); // 初始化顺序表
+int findMin(SeqList seq[]); // 初始化顺序表
+
 int main()
 {
-    
+    SeqList seq;
+    initSeq(&seq,MaxSize);
+    int min = findMin(&seq);
+    printf("顺序表中最小值是：%d, 下标为：%d\n", seq.datas[min], min);
+}
+
+void initSeq(SeqList seq[], int len) {
+    seq->length = 0; // 记得给的初始值
+    for (int i = 0; i < len; i++)
+    {
+        seq->datas[i] = rand()%95;
+        seq->length++;
+    }
+}
+
+int findMin(SeqList seq[]) {
+    int min = 0;
+    for (int i = 0; i < seq->length; i++)
+    {
+        if (seq->datas[i] < seq->datas[min]) {
+            min = i;
+        }
+    }
+    return min;
 }
---
+++ Chapter2.2/Chapter2.2.c
@@ -0,0 +1,30 @@
+﻿/*
+    用不多于3n/2的平均比较次数，在顺序表A[1..n]中分别找出最大元素和最小元素
+*/
+#include "stdio.h"
+#include <stdlib.h>
+
+typedef struct {
+    int datas;
+    int length;
+    int capacity;
+}SeqList;
+
+void initSeq(SeqList* seq);
+
+int main()
+{
+    SeqList list;
+    initSeq(&list);
+    printf("%d", list.datas[1]);
+}
+
+// 动态顺序表
+void initSeq(SeqList* seq) {
+    seq->length = 0;
+    seq->capacity = 10;
+    seq->datas = (SeqList*)malloc(seq->datas, seq->capacity * sizeof(SeqList));
+    for (int i = 0; i < seq->capacity; i++) {
+        seq->datas[i] = rand() % 20;
+    }
+}
