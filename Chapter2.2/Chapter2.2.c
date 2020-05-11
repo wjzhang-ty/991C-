cdc530840f68eec71f19fc03f1323fad67d3f3e6
wjzhang <2426477795@qq.com>
2020-05-11 15:18:59 +0800

    第二题

--- Chapter2.2/Chapter2.2.c
+++ Chapter2.2/Chapter2.2.c
@@ -5,26 +5,42 @@
 #include <stdlib.h>
 
 typedef struct {
-    int datas;
+    int* datas;
     int length;
     int capacity;
 }SeqList;
 
 void initSeq(SeqList* seq);
+void findLimit(SeqList seq);    // 不需要处理链表所以不用*
 
 int main()
 {
     SeqList list;
     initSeq(&list);
-    printf("%d", list.datas[1]);
+    findLimit(list);
 }
 
 // 动态顺序表
 void initSeq(SeqList* seq) {
     seq->length = 0;
     seq->capacity = 10;
-    seq->datas = (SeqList*)malloc(seq->datas, seq->capacity * sizeof(SeqList));
+    seq->datas = (int*)malloc(seq->capacity * sizeof(int)); // 动态申请内存
     for (int i = 0; i < seq->capacity; i++) {
         seq->datas[i] = rand() % 20;
+        seq->length++;
     }
+}
+
+void findLimit(SeqList seq) {
+    int minIndex = 0, maxIndex = 0;
+    for (int i = 0; i < seq.length; i++) {
+        if (seq.datas[i] > seq.datas[maxIndex]) {
+            maxIndex = i;
+        }
+        if (seq.datas[i] < seq.datas[minIndex]) {
+            minIndex = i;
+        }
+    }
+    printf("最大值：seq.datas[%d]=%d\n", maxIndex, seq.datas[maxIndex]);
+    printf("最小值：seq.datas[%d]=%d\n", minIndex, seq.datas[minIndex]);
 }
