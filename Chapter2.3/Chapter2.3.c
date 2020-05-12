dee1eb75fd0998db0068f1230667a559c7f85087
wjzhang <2426477795@qq.com>
2020-05-12 17:35:19 +0800

    第二章3 4题

---
+++ Chapter2.3/Chapter2.3.c
@@ -0,0 +1,57 @@
+﻿/*
+    已知长度为n的线性表A采用顺序存储结构，表中均为int数据。
+    请写出该顺序表中查找值为item的数据元素的递归算法。
+    查找成功返回位置，失败返回-1。
+*/
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int datas[10];
+    int length;
+}SeqList;
+
+void initSeq(SeqList* seq); // 初始化
+int searchSeq(SeqList seq,int val,int index); // 查
+
+int main()
+{
+    SeqList seq;
+    int val;
+    int index;
+
+    printf("请输入您要查找的元素值（1-11）");
+    scanf("%d", &val);
+
+    initSeq(&seq);
+    index = searchSeq(seq,val,0);
+
+    printf("所查找的值为%d，位于第%d个元素", val, index);
+}
+
+void initSeq(SeqList* seq) {
+    seq->length = 0;
+    for (int i = 0; i < 10; i++) {
+        seq->datas[i] = i+1;
+        seq->length++;
+    }
+}
+
+/*
+    @param seq 顺序表
+    @param val 查找值
+    @param index 所查找的下标
+*/
+int searchSeq(SeqList seq,int val,int index) {
+    if (index > seq.length) {
+        return -1;
+    }
+    else if (seq.datas[index] == val) {
+        return index;
+    }
+    else {
+        return searchSeq(seq, val, ++index);
+    }
+}
---
+++ Chapter2.4/Chapter2.4.c
@@ -0,0 +1,34 @@
+﻿/*
+    已知长度为n的线性表A采用顺序存储结构。请写出逆转该线性表的算法
+    要求在逆转过程中用最少的附加空间（尽可能少的辅助变量）
+*/
+#include <stdio.h>
+
+void myReplace(int* seq, int len); // 我的转置
+void printSeq(int seq[], int len); // 打印数组
+
+int main()
+{
+    int seq[10] = { 1,2,3,4,5,6,7,8,9,10 }; // 只要内存中连续都叫顺序表
+    int len = sizeof(seq) / sizeof(seq[0]); // sizeof获取的是内存长度，总长/单个长=数量
+    printSeq(seq, len);
+    myReplace(&seq, len);
+    printSeq(seq, len);
+}
+
+void myReplace(int* seq, int len) {
+    int a;
+    // 9/2=4(向下取整)
+    for (int i = 0; i < len / 2; i++) {
+        a = seq[i];
+        seq[i] = seq[len - i - 1];
+        seq[len - i - 1] = a;
+    }
+}
+
+void printSeq(int seq[], int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%d ", seq[i]);
+    }
+    printf("\n");
+}
